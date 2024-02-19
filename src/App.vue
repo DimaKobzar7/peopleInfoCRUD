07dd67562e6cda1179225fcca4b78f2990fa77d7
Dima Kobzar <>
2024-02-19 21:36:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainPage from "@/pages/MainPage.vue";
+</script>
+
+<template>
+  <MainPage />
+</template>
+
+<style lang="scss">
+@import "./assets/styles/imports";
+</style>
